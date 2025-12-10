73e8e591fbbc2ba979d297ba110c25f2ee1787a8
Duda Sena <duda.sruiz@gmail.com>
2025-12-10 11:04:49 -0300

    exercício simples 2

---
+++ src/Dia2_ValoresDeX.java
@@ -0,0 +1,18 @@
+public class Dia2_ValoresDeX {
+    public static void main(String[] args) {
+
+        //atv: programa que o valor não é escolhido pelo usuário. Se x for par recebe
+        //ele somado com 5, se for ímpar recebe ele multiplicado por 2
+        //usando if-else
+
+        int x = 17;
+
+        if (x % 2 == 0) {
+            x = x + 5;
+            System.out.println("Par: " + x);
+        } else {
+            x = x * 2;
+            System.out.println("Ímpar: " + x);
+        }
+    }
+}
