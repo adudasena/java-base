6d6d40b9e1e44c54a6d6ee0eeee9f12d2b65bb60
Duda Sena <duda.sruiz@gmail.com>
2026-01-28 01:52:54 -0300

    Exercício 14 adc e testado

---
+++ src/Ex14_Student.java
@@ -0,0 +1,54 @@
+public class Ex14_Student {
+    public static class Student {
+
+        private String name;
+        private double grade1;
+        private double grade2;
+
+        //construtor
+        public Student(String name, double grade1, double grade2) {
+            this.name = name;
+            this.grade1 = grade1;
+            this.grade2 = grade2;
+        }
+
+        //getters e setters
+        public String getName() {
+            return name;
+        }
+
+        public double getGrade1() {
+            return grade1;
+        }
+
+        public double getGrade2() {
+            return grade2;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setGrade1(double grade1) {
+            this.grade1 = grade1;
+        }
+
+        public void setGrade2(double grade2) {
+            this.grade1 = grade2;
+        }
+
+        //métodos
+        public double calculateAverage() {
+            return (grade1 * grade2) / 2;
+        }
+
+        public boolean isApproved() {
+            if ((grade1 * grade2) / 2 >= 7.0) {
+                return true;
+            } else {
+                return false;
+        }
+
+    }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Ex12_User.User user1= new Ex12_User.User("Maria", "maria@gmail.com", 20);
+        Ex12_User.User user1 = new Ex12_User.User("Maria", "maria@gmail.com", 20);
         System.out.println(user1.isAdult());
-
-        Ex13_Product.Product product1= new Ex13_Product.Product ("Batom vermelho", 35.0, 5);
-
+        System.out.println("----------");
+        Ex13_Product.Product product1 = new Ex13_Product.Product("Batom vermelho", 35.0, 5);
         System.out.println(product1.isAvailable());
         System.out.println(product1.totalValue());
+        System.out.println("----------");
 
+        Ex14_Student.Student student = new Ex14_Student.Student("João", 50, 90);
+        System.out.println(student.calculateAverage());
+        System.out.println(student.isApproved());
     }
 }
