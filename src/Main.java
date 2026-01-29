86d1da8081d171706e1ff5378d9815394106a011
Duda Sena <duda.sruiz@gmail.com>
2026-01-29 17:55:05 -0300

    Percorrendo a lista

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,13 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        Ex12_User.User user1 = new Ex12_User.User("Maria", "maria@gmail.com", 20);
-        System.out.println(user1.isAdult());
-        System.out.println("----------");
+        System.out.println("---A partir daqui, classe Product e seus testes---");
         Ex13_Product.Product product1 = new Ex13_Product.Product("Batom vermelho", 35.0, 5);
         System.out.println(product1.isAvailable());
         System.out.println(product1.totalValue());
         System.out.println("----------");
 
+        System.out.println("---A partir daqui, classe estudante e seus testes---");
         Ex14_Student.Student student1 = new Ex14_Student.Student("Fulano", 40.0, 20.0);
         System.out.println(student1.calculateAverage());
         System.out.println(student1.isApproved());
@@ -24,7 +23,15 @@ public class Main {
         students.add(student2);
         students.add(student3);
 
+        System.out.println(students.get(0).getName());
 
+        System.out.println("--Percorrendo a lista--");
+        for (Ex14_Student.Student s : students) {
+            System.out.println("Aluno: " + s.getName());
+            System.out.println("Média: " + s.calculateAverage());
+            System.out.println("Aprovado: " + s.isApproved());
+            System.out.println("-----");
+        }
 
     }
 }
